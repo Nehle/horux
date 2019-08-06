082f851adac04e3e5296dd17af3c96bd52f009e3
Jakob Nilsson-Ehle <jakob.nilsson-ehle@jayway.com>
2019-08-06 16:30:15 +0200

    Update dependencies

--- test/filter.spec.js
+++ test/filter.spec.js
@@ -1,21 +1,21 @@
-import sinon from "sinon";
-import chai, { expect } from "chai";
-import sinonChai from "sinon-chai";
-import { filter, linkIf } from "../lib";
+import sinon from 'sinon';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import { filter, linkIf } from '../lib';
 
 chai.use(sinonChai);
 
-describe("filter", () => {
-  it("is exported properly", () => {
-    expect(filter).to.be.a("function");
+describe('filter', () => {
+  it('is exported properly', () => {
+    expect(filter).to.be.a('function');
   });
 
-  it("has the linkIf alias", () => {
+  it('has the linkIf alias', () => {
     expect(filter).to.eql(linkIf);
   });
 
   it('throws an error if "predicate" is not a function', () => {
-    expect(() => filter()).to.throw("not a function");
+    expect(() => filter()).to.throw('not a function');
   });
 
   it('calls "next" if the predicate returns truthy', () => {
